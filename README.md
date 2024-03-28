24e1a470212760ffd4b3089560304ecc157d9119
seanveal <seand.veal@yahoo.com>
2024-03-28 12:17:47 -0500

    init: ReadME and Project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Swipe-Actions-ScrollView-iOS17
-Following another tutorial for swipe actions
+Following [another tutorial](https://youtube.com/watch?v=K8VnH2eEnK4) for swipe actions
---
+++ SwipeActionsiOS17/SwipeActionsiOS17/SwipeActionsiOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionsiOS17App.swift
+//  SwipeActionsiOS17
+//
+//  Created by Sean Veal on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionsiOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
